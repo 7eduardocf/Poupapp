d4d8e00742ff17d09feafba6c0b68e5d052ee01c
7eduardocf <eduardocfrota901@gmail.com>
2025-06-08 22:31:13 -0400

    Implementado os metodos no formulario de cadastro

--- src/screens/Cadastro/index.tsx
+++ src/screens/Cadastro/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import {
-  Section,
-  Container,
-  Title,
-  Description,
-  Illustration,
-  SectionWrapper,
+    Section,
+    Container,
+    Title,
+    Description,
+    Illustration,
+    SectionWrapper,
 } from "./style.js";
 import ilustracao from "../../assets/images/ilustracao-cadastro.png";
 import { Form, useNavigate } from "react-router";
@@ -13,59 +13,77 @@ import Botao from "../../componentes/Botao/index.js";
 import CampoTexto from "../../componentes/CampoTexto/index.js";
 import Fieldset from "../../componentes/Fieldset/index.js";
 import Label from "../../componentes/Label/index.js";
+import { IUsuario } from "../../types/index.js";
+import { criarUsuario } from "../../api/index.js";
 
 const Cadastro = () => {
-  const [nome, setNome] = useState("");
-  const [renda, setRenda] = useState("");
+    const [form, setForm] = useState<Omit<IUsuario, "id">>({
+        nome: "",
+        renda: 0
+    })
 
-  const navigate = useNavigate();
+    const aoDigitarNoCampo = (campo: "nome"|"renda", valor: string)=>{
+        setForm((prev)=>({...prev, [campo]: valor}))
+    }
 
-  const aoSubmeterFormulario = (evento: React.FormEvent) => {
-    evento.preventDefault();
-    navigate("/home");
-  };
+    const navigate = useNavigate();
 
-  return (
-    <Section>
-      <SectionWrapper>
-        <Container>
-          <Title>Configuração financeira</Title>
-          <Description>
-            Boas-vindas à plataforma que protege seu bolso! Antes de começar,
-            precisamos de algumas informações sobre sua rotina financeira. Vamos
-            lá?
-          </Description>
-          <Form>
-            <Fieldset>
-              <Label htmlFor="nome">Nome</Label>
-              <CampoTexto
-                type="text"
-                name="nome"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-              />
-            </Fieldset>
-            <Fieldset>
-              <Label htmlFor="renda">Renda mensal total</Label>
-              <CampoTexto
-                type="text"
-                name="renda"
-                value={renda}
-                onChange={(e) => setRenda(e.target.value)}
-              />
-            </Fieldset>
-          </Form>
-          <Botao $variante="primario" onClick={aoSubmeterFormulario}>
-            Ir para o app
-          </Botao>
-        </Container>
-        <Illustration
-          src={ilustracao}
-          alt="ilustração da tela de cadastro. Um avatar mexendo em alguns gráficos"
-        />
-      </SectionWrapper>
-    </Section>
-  );
+    const aoSubmeterFormulario = async(evento: React.FormEvent) => {
+        evento.preventDefault();
+        try {
+            
+            const novoUsuario = await criarUsuario(form)
+            console.log(novoUsuario);
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+        navigate("/home");
+    };
+
+    return (
+        <Section>
+            <SectionWrapper>
+                <Container>
+                    <Title>Configuração financeira</Title>
+                    <Description>
+                        Boas-vindas à plataforma que protege seu bolso! Antes de começar,
+                        precisamos de algumas informações sobre sua rotina financeira. Vamos
+                        lá?
+                    </Description>
+                    <Form>
+                        <Fieldset>
+                            <Label htmlFor="nome">Nome</Label>
+                            <CampoTexto
+                                type="text"
+                                name="nome"
+                                value={form.nome}
+                                onChange={(e:React.ChangeEvent<HTMLInputElement>) => aoDigitarNoCampo("nome", e.target.value)}
+                            />
+                        </Fieldset>
+                        <Fieldset>
+                            <Label htmlFor="renda">Renda mensal total</Label>
+                            <CampoTexto
+                                type="text"
+                                name="renda"
+                                value={form.renda}
+                                onChange={(e:React.ChangeEvent<HTMLInputElement>) => aoDigitarNoCampo("renda",e.target.value)}
+                            />
+                        </Fieldset>
+                    </Form>
+                    <Botao $variante="primario" onClick={aoSubmeterFormulario}>
+                        Ir para o app
+                    </Botao>
+                </Container>
+                <Illustration
+                    src={ilustracao}
+                    alt="ilustração da tela de cadastro. Um avatar mexendo em alguns gráficos"
+                />
+            </SectionWrapper>
+        </Section>
+    );
 };
 
 export default Cadastro;
