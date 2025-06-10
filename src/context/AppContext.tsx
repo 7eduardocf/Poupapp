30a9a4c6fa66da41e31ad498678ddb2767f7e6fb
7eduardocf <eduardocfrota901@gmail.com>
2025-06-09 22:40:37 -0400

    Iniciando o estado do usuario

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,18 +1,48 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
 import { IUsuario } from "../types"
+import { obterUsuario } from "../api"
 
-interface AppContextType{
+interface AppContextType {
     usuario: IUsuario | null
+    criarUsuario: (usuario:Omit<IUsuario,"id">) => Promise<void>
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined)
 
-const AppProvider = ({children}:{children: React.ReactNode}) => {
-  return (
-    <AppContext.Provider value={{}}>
-        {children}
-    </AppContext.Provider>
-  )
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const [usuario, setUsuario] = useState<IUsuario | null>(null)
+
+    const carregaDadosUsuario = async()=>{
+        try {
+            const usuarios = await obterUsuario()
+            if(usuarios.length > 0){
+                setUsuario(usuarios[0])
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+
+    useEffect(()=>{
+        carregaDadosUsuario()
+    },[])
+
+    const criarUsuario = async(usuario:Omit<IUsuario, "id">)=>{
+        try {
+            const novoUsuario = await criarUsuario(usuario)
+            setUsuario(novoUsuario)
+        } catch (error) {
+            console.log(error);
+               
+        }
+    }
+
+    return (
+        <AppContext.Provider value={{usuario, criarUsuario}}>
+            {children}
+        </AppContext.Provider>
+    )
 }
 
 export default AppProvider
